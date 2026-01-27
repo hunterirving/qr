a34ab55616b02d6d76df7f35f5e5bd5323248d36
Hunter <hunter@hunterirving.com>
2026-01-26 23:32:39 -0500

    initial commit

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'qr-v1';
+const urlsToCache = [
+  '/qr/',
+  '/qr/index.html',
+  '/qr/manifest.json',
+  '/qr/app_icon.png'
+];
+
+// Install service worker and cache resources
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate service worker and clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch strategy: Cache first, then network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(response => {
+            if (response.status === 200) {
+              const responseClone = response.clone();
+              caches.open(CACHE_NAME)
+                .then(cache => cache.put(event.request, responseClone));
+            }
+            return response;
+          });
+      })
+  );
+});
